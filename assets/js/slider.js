761aba39404191a44442a4e2c8289c75b5c8b0f7
Mohamed <mo.magdyabdelsalam@gmail.com>
2024-10-18 01:31:49 +0300

    sliding improvement

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,34 +1,39 @@
 //slider
 function sliding(){
+    const slidesContainer = document.querySelector('.slider ul');
+    const slides = [...slidesContainer.children];
+
+    function moveToSlide(offset){
+        const activeSlide = document.querySelector('[data-active]');
+        let newIndex = slides.indexOf(activeSlide) + offset;
+        if(newIndex >= slides.length) newIndex = 0;
+        if(newIndex < 0) newIndex = slides.length - 1;
+        slides[newIndex].dataset.active = true;
+        delete activeSlide.dataset.active;
+    }
+    
     let interval; 
     function startInterval(){
         interval = setInterval(()=>{
-            const slidesContainer = document.querySelector('.slider ul');
-            const activeSlide = slidesContainer.querySelector('[data-active]');
-            let newIndex = [...slidesContainer.children].indexOf(activeSlide) + 1;
-            if(newIndex >= slidesContainer.children.length) newIndex = 0;
-            slidesContainer.children[newIndex].dataset.active = true;
-            delete activeSlide.dataset.active;
+            moveToSlide(1)
         }, 6000)
     }
+    
     function stopInterval(){
         clearInterval(interval)
     }
+
     startInterval();
     const sliderButtons = document.querySelectorAll('[data-slider-button]');
     sliderButtons.forEach(el => {
         el.addEventListener('click', ()=>{
             stopInterval()
-            const slidesContainer = document.querySelector('.slider ul');
-            const activeSlide = slidesContainer.querySelector('[data-active]');
             const offset = el.className === 'next' ? 1 : -1;
-            let newIndex = [...slidesContainer.children].indexOf(activeSlide)+offset;
-            if(newIndex >= slidesContainer.children.length) newIndex = 0;
-            if(newIndex < 0) newIndex = slidesContainer.children.length - 1;
-            slidesContainer.children[newIndex].dataset.active = true;
-            delete activeSlide.dataset.active;
+            moveToSlide(offset)
             startInterval()
-        })
-    })
+        });
+    });
 }
+
+
 sliding();
